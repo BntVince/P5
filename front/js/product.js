01306f8eedaaa7364276a5907cb8fa97bc5f9aae
Bnt Vince <bnt.vincent31@gmail.com>
2022-06-12 17:42:51 +0200

    Product.js fonctionnel

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+let urlId
 fetch("http://localhost:3000/api/products")
 .then(function (app) {
     if (app.ok) {
@@ -5,37 +6,86 @@ fetch("http://localhost:3000/api/products")
     }
 })
 .then(function (items) {
-   //console.log(items);
+    // Ici on récupère l'url de la page actuel et isole l'élement id dans une variable urlId
     const str = document.location.href;
     const url =  new URL(str);
-    const urlId = url.searchParams.get("id");
-   //console.log(urlId);
-
+    urlId = url.searchParams.get("id");
+   
+    // On cherche une correspondance avec l'urlId et l'_id des produits contenue dans l'API
    for (item of items) {
         if (item._id === urlId) {
             rightItem = item;
             break;
         }
     };
-
-    itemImg = document.createElement('img');
+    document.title = rightItem.name; // On modifi la balise title pour quelle corresponde au nom de l'article
+    // On définie l'image et lui donne ses attributs src et alt
+    let itemImg = document.createElement('img');
     itemImg.src = rightItem.imageUrl;
     itemImg.alt = rightItem.altTxt;
-
-
+    // On créer les éléments img, title, price et description dans la page produit
     document.querySelector('article .item__img').append(itemImg);
     document.querySelector('.item__content__titlePrice #title').innerText = rightItem.name;
     document.querySelector('.item__content__titlePrice #price').innerText = rightItem.price;
     document.querySelector('.item__content__description #description').innerText = rightItem.description;
 
-
+    // Ici on créer une boucle qui va parcourir toute les valeurs possible de color pour intégrer chaqu'une d'elle
+    // dans le menu déroulant 
     for (color of rightItem.colors) {
         itemOption = document.createElement('option');
         itemOption.value = color;
         itemOption.innerText = color;
         document.querySelector('.item__content__settings #colors').append(itemOption);
-        
     };
+});
 
-    //console.log(rightItem);
+let activeColor = ""; // On défini une varriable et on récupère sa valeur (l45-l48)
+let quantity = 0; // On défini une varriable et on récupère sa valeur (l50-l53)
+
+document.getElementById('colors').addEventListener('change', function () {
+    activeColor = this[this.selectedIndex].value;
+    //console.log(activeColor);
+});
+
+document.getElementById('quantity').addEventListener('change', function() {
+    activeQuantity = this.valueAsNumber;
+    //console.log(quantity);
+})
+// On créer un constructor pour les ajouts dans le panier 
+class addToCart {
+    constructor(id, quantity, color) {
+        this.id = id;
+        this.quantity = quantity;
+        this.color = color;
+    }
+}
+//Quand on click sur Ajouter au panier
+document.getElementById('addToCart').addEventListener('click', function() {
+    //Si aucune couleur ou aucune quantité à ajouter n'est sélectionner 
+    if(activeColor == "" || activeQuantity == 0) {
+        alert("Veuillez sélectionner une couleur et le nombre de Kanap que vous souhaitez commander")
+    }else{
+       
+        let panier = []; // On déclare panier qui va contenir sous forme de tableau les objets
+        let addKanap; // On déclare l'objet qui sera ajouté au panier si ajout il y a
+        let itemUpdate = false; // On créer une variable qui nous servira à déterminer si au click on créer un nouvelobjet ou l'on met à jour un déja existant
+        if(sessionStorage.panier) { // Si un panier existe déja dans le sessionStorage
+            panier = JSON.parse(sessionStorage.panier); // On le récupère et l'associe au tableau panier créer (l69)
+
+            for (kanap of panier) { // Pour chaque objet déja présent dans le panier on regarde 
+                if(kanap.id == urlId && kanap.color == activeColor) { // Si l'objet rajouter existe déja dans celui-ci au quel cas 
+                    kanap.quantity += activeQuantity; // On modifie la quantité de l'objet du panier pour  y rajouter la valeur sélectionner 
+                    itemUpdate = true; // On défini que l'on a modifier un objet
+                }
+            }
+            if(!itemUpdate) { // Si on n'a pas modifier 
+            addKanap = new addToCart (urlId, activeQuantity, activeColor); // On créer un nouvelle objet à ajouter
+            panier.push(addKanap); // On met l'objet dans le panier
+            }
+        }else{
+            addKanap = new addToCart (urlId, activeQuantity, activeColor); // On créer un nouvelle objet à ajouter
+            panier.push(addKanap); // On met l'objet dans le panier
+        }
+    sessionStorage.setItem("panier", JSON.stringify(panier)) // Pour finir on stock le panier dans sessionStorage
+    }
 })
--- front/js/script.js
+++ front/js/script.js
@@ -7,30 +7,30 @@ fetch("http://localhost:3000/api/products")
 .then(function (items) {
     //console.log(items);
 
-    for(item of items) {
-
-        itemLink = document.createElement('a');
+    for(item of items) { // pour chaque élément du array que retourne l'API =>
+        //On défini le lien et son href
+        let itemLink = document.createElement('a');
         itemLink.href = "./product.html?id=" + item._id;
-
-        itemArticle = document.createElement('article');
-        
-        itemImg = document.createElement('img');
+        //On défini l'article
+        let itemArticle = document.createElement('article');
+        //On défini l'image, ses attribut src et alt
+        let itemImg = document.createElement('img');
         itemImg.src = item.imageUrl;
         itemImg.alt = item.altTxt;
-
-        itemTitle = document.createElement('h3');
+        // On défini le titre, sa class et le texte qu'il contient
+        let itemTitle = document.createElement('h3');
         itemTitle.classList = item.name;
         itemTitle.innerText = item.name;
-
-        itemParagraphe = document.createElement('p');
+        //On défini un Paragraphe, sa class et la description qui va contenir
+        let itemParagraphe = document.createElement('p');
         itemParagraphe.classList = "productDescription";
         itemParagraphe.innerText = item.description;
 
-
+        //On sélectionne l'élément dans le quel on créer un lien ("itemLink l12-13")
         document.getElementById('items').append(itemLink);
-
+        //On créer un article ("itemArticle l15") dans le lien créer plus haut 
         itemLink.append(itemArticle);
-        
+        //On créer une Image, un Titre et un Paragraphe ("itemImg l17-19", "itemTitle l21-23", "itemParagraphe l25--27" )
         itemArticle.append(itemImg, itemTitle, itemParagraphe);
 
     }
