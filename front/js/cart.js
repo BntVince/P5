2870b6b2cb789bf57051acffd2b2c0f8681a0f46
Bnt Vince <bnt.vincent31@gmail.com>
2022-06-17 16:32:48 +0200

    Optimisation du code (moins de boucle) de product.js & cart.js reste formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,4 @@
+//--------------------------- UTILISABLE ---------------------------//
 const cartItems = document.getElementById('cart__items'); // On définie cartItems, l'élément HTML qui va contenir les article du panier
 let totalQuantity = 0 // On définie une variable qui va stocker le total d'article du panier
 let totalPrice = 0 // On définie une variable qui va stocker le total du prix du panier
@@ -5,130 +6,100 @@ function setAttributes(element, attributes) { // On créer une fonction qui va n
     for(var key in attributes) {
       element.setAttribute(key, attributes[key]);
     }
-  }
+}
 function writeTot() { // On créer une fonction qui va modifier sur la pages les valeurs afficher devant correspondre aux total d'article et du prix total
     document.getElementById('totalQuantity').innerText = totalQuantity
     document.getElementById('totalPrice').innerText = totalPrice
-}
-  
-fetch("http://localhost:3000/api/products")
-.then(function (app) {
-    if (app.ok) {
-        return app.json();
-    }
-})
-.then(function (items) {
+}   
+//------------------------------ CODE ------------------------------//
+if (!sessionStorage.panier || !JSON.parse(sessionStorage.panier)[0]) { // Si le panier n'existe pas ou est vide
+
+    cartItems.innerText = "Votre panier est vide !" // On affiche ça
+}else{ // si panier existe
+
+    panier = JSON.parse(sessionStorage.panier) // On le transforme en Array utilisable 
     
 
-    if (!sessionStorage.panier || !JSON.parse(sessionStorage.panier)[0]) { // Si le panier n'existe pas ou est vide
-
-        cartItems.innerText = "Votre panier est vide !" // On affiche ça
-    }else{ // si panier existe
-
-        let panier = JSON.parse(sessionStorage.panier) // On le transforme en Array utilisable 
+    for (let kanapInPanier of panier) {  // On parcour le panier
         
-
-        for (let kanapInPanier of panier) {  // On parcour le panier
-            for (let item of items) { // On parcour l'app
-                if (kanapInPanier.id == item._id) { // On cherche les correspondance et pour chaque correspondance on créer les article en HTML avec leurs class et attributs 
-
-                    let itemArticle = document.createElement('article');
-                    let cartItemImg = document.createElement('div');
-                    let itemImg = document.createElement('img');
-
-                    let cartItemContent = document.createElement('div')
-                    let cartItemContentDescription = document.createElement('div')
-                    let cartItemContentSettings = document.createElement('div')
-                    let cartItemContentSettingsQuantity = document.createElement('div')
-                    let cartItemContentSettingsDelete = document.createElement('div')
-                    let itemQuantity = document.createElement('input')
-                    let deleteItem = document.createElement('p')
-
-                    setAttributes(itemArticle, {'data-id': item._id, 'data-color': kanapInPanier.color});
-                    itemArticle.classList = 'cart__item';
-
-                    cartItemImg.classList = 'cart__item__img';
-
-                    itemImg.setAttribute('src', item.imageUrl), ('alt', item.altTxt);
-
-                    cartItemContent.classList = 'cart__item__content'
-                    cartItemContentDescription.classList = 'cart__item__content__description'
-                    cartItemContentDescription.innerHTML += "<h2>"+item.name+"</h2><p>"+kanapInPanier.color+"</p><p>"+item.price+"</p>"
-                    cartItemContentSettings.classList = 'cart__item__content__settings'
-                    cartItemContentSettingsQuantity.classList = 'cart__item__content__settings__quantity'
-                    cartItemContentSettingsQuantity.innerHTML = '<p>Qté : '+kanapInPanier.quantity+'</p>'
-                    setAttributes(itemQuantity, {'type': 'number', 'name': 'itemQuantity', 'min': '1', 'max': '100', 'value': kanapInPanier.quantity})
-                    itemQuantity.classList = 'itemQuantity'
-                    cartItemContentSettingsDelete.classList = 'cart__item__content__settings__delete'
-                    deleteItem.innerText = 'Supprimer'
-                    deleteItem.classList = 'deleteItem'
-
-                    cartItemImg.append(itemImg)
-                    cartItemContentSettingsQuantity.append(itemQuantity)
-                    cartItemContentSettingsDelete.append(deleteItem)
-                    cartItemContentSettings.append(cartItemContentSettingsQuantity, cartItemContentSettingsDelete)
-                    cartItemContent.append(cartItemContentDescription, cartItemContentSettings)
-                    itemArticle.append(cartItemImg, cartItemContent)
-
-                    cartItems.append(itemArticle);
-                    // Une fois créer , pour chaque kanap du panier et affiché 
-                    totalQuantity += kanapInPanier.quantity // On rajoute le nombre de kanap du panier 
-                    totalPrice += item.price * kanapInPanier.quantity // On rejoute le total du ou des kanap du panier
-                    writeTot() // On écrit les valeurs obtenu
-                    break;
-                }
+        fetch("http://localhost:3000/api/products/"+kanapInPanier.id)
+        .then(function (app) {
+            if (app.ok) {
+                return app.json();
             }
+        })
+        .then(function (item){
 
+            let itemArticle = document.createElement('article');
+            let cartItemImg = document.createElement('div');
+            let itemImg = document.createElement('img');
 
+            let cartItemContent = document.createElement('div')
+            let cartItemContentDescription = document.createElement('div')
+            let cartItemContentSettings = document.createElement('div')
+            let cartItemContentSettingsQuantity = document.createElement('div')
+            let cartItemContentSettingsDelete = document.createElement('div')
+            let itemQuantity = document.createElement('input')
+            let deleteItem = document.createElement('p')
 
-        }
-        let itemsQuantity = document.querySelectorAll('.itemQuantity')
-        for (let inputQuantity of itemsQuantity) { // On défini inputQuantity parmis toutes les différentes inputsQuantity de la page
-            inputQuantity.addEventListener('change', function() { // Au changement de value de l'un de ceux-ci
-                for (let kanap of panier) { // On parcour le panier 
-                    if (kanap.id == this.closest('article').dataset.id && kanap.color == this.closest('article').dataset.color) { // Pour trouver la correspondance avec l'article
-                        totalQuantity -= kanap.quantity 
-                        totalQuantity += this.valueAsNumber // On redéfinie la quantité totals
-                        for (let item of items) {
-                            if (kanap.id == item._id) { // On parcour l'app pour trouver le prix de l'article
-                                totalPrice -= item.price * kanap.quantity
-                                totalPrice += item.price * this.valueAsNumber // Et redéfinir le prix total
-                                break
-                            }
-                        }
-                        kanap.quantity = this.valueAsNumber // On redéfinie la quantité dans le panier et on modifie la valeurs affiché
-                        this.closest('article').querySelector('.cart__item__content__settings__quantity p').innerText = 'Qte : ' + inputQuantity.valueAsNumber 
-                        writeTot() // On écrit les valeurs obtenu pour le nombres total d'article et le prix total ( que l'on a modifié l90-95)
-                        sessionStorage.setItem('panier', JSON.stringify(panier)) // On modifie également le panier présent dans le sessionStorage pour que ce soit enregistré
-                        break
-                    }
+            setAttributes(itemArticle, {'data-id': item._id, 'data-color': kanapInPanier.color});
+            itemArticle.classList = 'cart__item';
+
+            cartItemImg.classList = 'cart__item__img';
+
+            itemImg.setAttribute('src', item.imageUrl), ('alt', item.altTxt);
+
+            cartItemContent.classList = 'cart__item__content'
+            cartItemContentDescription.classList = 'cart__item__content__description'
+            cartItemContentDescription.innerHTML += "<h2>"+item.name+"</h2><p>"+kanapInPanier.color+"</p><p>"+item.price+"</p>"
+            cartItemContentSettings.classList = 'cart__item__content__settings'
+            cartItemContentSettingsQuantity.classList = 'cart__item__content__settings__quantity'
+            cartItemContentSettingsQuantity.innerHTML = '<p>Qté : '+kanapInPanier.quantity+'</p>'
+            setAttributes(itemQuantity, {'type': 'number', 'name': 'itemQuantity', 'min': '1', 'max': '100', 'value': kanapInPanier.quantity})
+            itemQuantity.classList = 'itemQuantity'
+            cartItemContentSettingsDelete.classList = 'cart__item__content__settings__delete'
+            deleteItem.innerText = 'Supprimer'
+            deleteItem.classList = 'deleteItem'
+
+            cartItemImg.append(itemImg)
+            cartItemContentSettingsQuantity.append(itemQuantity)
+            cartItemContentSettingsDelete.append(deleteItem)
+            cartItemContentSettings.append(cartItemContentSettingsQuantity, cartItemContentSettingsDelete)
+            cartItemContent.append(cartItemContentDescription, cartItemContentSettings)
+            itemArticle.append(cartItemImg, cartItemContent)
+
+            cartItems.append(itemArticle);
+            // Une fois créer , pour chaque kanap du panier et affiché 
+            totalQuantity += kanapInPanier.quantity // On rajoute le nombre de kanap du panier 
+            totalPrice += item.price * kanapInPanier.quantity // On rejoute le total du ou des kanap du panier
+            writeTot() // On écrit les valeurs obtenu
+
+            itemQuantity.addEventListener('change', function() {  // Au changement de value de l'un de ceux-ci
+                
+                totalQuantity -= kanapInPanier.quantity 
+                totalQuantity += this.valueAsNumber // On redéfinie la quantité totals
+                totalPrice -= item.price * kanapInPanier.quantity
+                totalPrice += item.price * itemQuantity.valueAsNumber // Et redéfinir le prix total
+                        
+                kanapInPanier.quantity = itemQuantity.valueAsNumber // On redéfinie la quantité dans le panier et on modifie la valeurs affiché
+                itemQuantity.closest('article').querySelector('.cart__item__content__settings__quantity p').innerText = 'Qte : ' + itemQuantity.valueAsNumber 
+                writeTot() // On écrit les valeurs obtenu pour le nombres total d'article et le prix total ( que l'on a modifié l90-95)
+                sessionStorage.setItem('panier', JSON.stringify(panier)) // On modifie également le panier présent dans le sessionStorage pour que ce soit enregistré
+            })
+
+            deleteItem.addEventListener('click', function() { console.log(panier) // Au click sur l'un de ceux-ci
+        
+                totalQuantity -= kanapInPanier.quantity
+                totalPrice -= kanapInPanier.quantity * item.price // Et redéfinir le prix total
+                
+                deleteItem.closest('article').remove() // On supprime l'article affiché
+                writeTot() // On écrit les valeurs obtenu pour le nombres total d'article et le prix total ( que l'on a modifié l115-116)
+                panier.splice(panier.indexOf(kanapInPanier), 1) // On supprime l'article du panier
+                sessionStorage.setItem('panier', JSON.stringify(panier)) // On modifie le panier du sessionStorage
+                if (!sessionStorage.panier || !JSON.parse(sessionStorage.panier)[0]) { // On vérifie si après suppresion il reste des article dans le panier, sinon 
+                    cartItems.innerText = "Votre panier est vide !" // On affiche que le panier est vide
                 }
             })
-        }
-        let deleteItems = document.querySelectorAll('.deleteItem');
-        for (let deleteItem of deleteItems) { // On défini deleteItem parmis toutes les différentes inputsQuantity de la page
-            deleteItem.addEventListener('click', function() { // Au click sur l'un de ceux-ci
-                for (let kanap of panier) { // On parcour le panier 
-                    if (kanap.id == this.closest('article').dataset.id && kanap.color == this.closest('article').dataset.color) { // Pour trouver la correspondance avec l'article
-                        for (let item of items) { // On parcour l'app
-                            if (item._id == kanap.id) { // pour trouver le prix de l'article
-                                totalQuantity -= kanap.quantity
-                                totalPrice -= kanap.quantity * item.price // Et redéfinir le prix total
-                                break;
-                            }
-                        }
-                        this.closest('article').remove() // On supprime l'article affiché
-                        writeTot() // On écrit les valeurs obtenu pour le nombres total d'article et le prix total ( que l'on a modifié l115-116)
-                        panier.splice(panier.indexOf(kanap), 1) // On supprime l'article du panier
-                        sessionStorage.setItem('panier', JSON.stringify(panier)) // On modifie le panier du sessionStorage
-                        if (!sessionStorage.panier || !JSON.parse(sessionStorage.panier)[0]) { // On vérifie si après suppresion il reste des article dans le panier, sinon 
-                            cartItems.innerText = "Votre panier est vide !" // On affiche que le panier est vide
-                        }
-                        break
-                    }
-                }
-            })
-        }
+        })
     }
-})
-
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,56 +1,6 @@
-let urlId
-fetch("http://localhost:3000/api/products")
-.then(function (app) {
-    if (app.ok) {
-        return app.json();
-    }
-})
-.then(function (items) {
-    // Ici on récupère l'url de la page actuel et isole l'élement id dans une variable urlId
-    const str = document.location.href;
-    const url =  new URL(str);
-    urlId = url.searchParams.get("id");
-   
-    // On cherche une correspondance avec l'urlId et l'_id des produits contenue dans l'API
-   for (let item of items) {
-        if (item._id === urlId) {
-            rightItem = item;
-            break;
-        }
-    };
-    document.title = rightItem.name; // On modifi la balise title pour quelle corresponde au nom de l'article
-    // On définie l'image et lui donne ses attributs src et alt
-    let itemImg = document.createElement('img');
-    itemImg.src = rightItem.imageUrl;
-    itemImg.alt = rightItem.altTxt;
-    // On créer les éléments img, title, price et description dans la page produit
-    document.querySelector('article .item__img').append(itemImg);
-    document.querySelector('.item__content__titlePrice #title').innerText = rightItem.name;
-    document.querySelector('.item__content__titlePrice #price').innerText = rightItem.price;
-    document.querySelector('.item__content__description #description').innerText = rightItem.description;
-
-    // Ici on créer une boucle qui va parcourir toute les valeurs possible de color pour intégrer chaqu'une d'elle
-    // dans le menu déroulant 
-    for (color of rightItem.colors) {
-        itemOption = document.createElement('option');
-        itemOption.value = color;
-        itemOption.innerText = color;
-        document.querySelector('.item__content__settings #colors').append(itemOption);
-    };
-});
-
-let activeColor = ""; // On défini une varriable et on récupère sa valeur (l45-l48)
-let activeQuantity = 0; // On défini une varriable et on récupère sa valeur (l50-l53)
-
-document.getElementById('colors').addEventListener('change', function () {
-    activeColor = this.value;
-    //console.log(activeColor);
-});
-
-document.getElementById('quantity').addEventListener('change', function() {
-    activeQuantity = this.valueAsNumber;
-    //console.log(quantity);
-})
+let urlId // On définie une variable qui va contenir l'id du produit 
+let activeColor = ""; // On défini une varriable et on récupère sa valeur 
+let activeQuantity = 0; // On défini une varriable et on récupère sa valeur 
 // On créer un constructor pour les ajouts dans le panier 
 class addToCart {
     constructor(id, quantity, color) {
@@ -59,6 +9,50 @@ class addToCart {
         this.color = color;
     }
 }
+
+// Ici on récupère l'url de la page actuel et isole l'élement id dans la variable urlId
+const str = document.location.href;
+const url =  new URL(str);
+urlId = url.searchParams.get("id");
+
+// On appel directement le bon object dans l'API grace à id contenue dans l'url
+fetch("http://localhost:3000/api/products/"+urlId)
+.then(function (app) {
+    if (app.ok) {
+        return app.json();
+    }
+})
+.then(function (item) {
+
+    document.title = item.name; // On modifi la balise title pour quelle corresponde au nom de l'article
+    // On définie l'image et lui donne ses attributs src et alt
+    let itemImg = document.createElement('img');
+    itemImg.src = item.imageUrl;
+    itemImg.alt = item.altTxt;
+    // On créer les éléments img, title, price et description dans la page produit
+    document.querySelector('article .item__img').append(itemImg);
+    document.querySelector('.item__content__titlePrice #title').innerText = item.name;
+    document.querySelector('.item__content__titlePrice #price').innerText = item.price;
+    document.querySelector('.item__content__description #description').innerText = item.description;
+
+    // Ici on créer une boucle qui va parcourir toute les valeurs possible de color pour intégrer chaqu'une d'elle
+    // dans le menu déroulant 
+    for (color of item.colors) {
+        itemOption = document.createElement('option');
+        itemOption.value = color;
+        itemOption.innerText = color;
+        document.querySelector('.item__content__settings #colors').append(itemOption);
+    };
+});
+
+
+document.getElementById('colors').addEventListener('change', function () {
+    activeColor = this.value;
+});
+
+document.getElementById('quantity').addEventListener('change', function() {
+    activeQuantity = this.valueAsNumber;
+})
 //Quand on click sur Ajouter au panier
 document.getElementById('addToCart').addEventListener('click', function() {
     //Si aucune couleur ou aucune quantité à ajouter n'est sélectionner 
@@ -70,7 +64,7 @@ document.getElementById('addToCart').addEventListener('click', function() {
         let addKanap; // On déclare l'objet qui sera ajouté au panier si ajout il y a
         let itemUpdate = false; // On créer une variable qui nous servira à déterminer si au click on créer un nouvelobjet ou l'on met à jour un déja existant
         if(sessionStorage.panier) { // Si un panier existe déja dans le sessionStorage
-            panier = JSON.parse(sessionStorage.panier); // On le récupère et l'associe au tableau panier créer (l69)
+            panier = JSON.parse(sessionStorage.panier); // On le récupère et l'associe au tableau panier créer 
 
             for (kanap of panier) { // Pour chaque objet déja présent dans le panier on regarde 
                 if(kanap.id == urlId && kanap.color == activeColor) { // Si l'objet rajouter existe déja dans celui-ci au quel cas 
@@ -78,7 +72,7 @@ document.getElementById('addToCart').addEventListener('click', function() {
                     itemUpdate = true; // On défini que l'on a modifier un objet
                 }
             }
-            if(!itemUpdate) { // Si on n'a pas modifier 
+            if(!itemUpdate) { // Si on n'a pas modifier d'objet
             addKanap = new addToCart (urlId, activeQuantity, activeColor); // On créer un nouvelle objet à ajouter
             panier.push(addKanap); // On met l'objet dans le panier
             }
